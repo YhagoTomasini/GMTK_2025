a84a6052189cd9d6660ea3e37ff2ea2afe0328d7
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-07-30 18:46:57 -0300

    teste

--- Scripts/aranhinha.gd
+++ Scripts/aranhinha.gd
@@ -2,13 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+var started : bool = false
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		started = true
+		
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if started:
+		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		
+		
 	input_vector = input_vector.normalized()
 	
 	velocity = input_vector * SPEED
