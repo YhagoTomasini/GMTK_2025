954f0fc9aacfedf0ef22f704661e8a7ebd75d3d5
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-07-30 20:13:12 -0300

    update cuspir

--- Scripts/aranhinha.gd
+++ Scripts/aranhinha.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+const sceneTeia = preload("res://Scene/teia.tscn")
 @onready var cam: Camera2D = $"../Camera2D"
 
-var spRotation : float = 5
+var speedRotation : float = 5
 
 var cdTeia : float = 5
-var SPEED = 300.0
+var speed = 300.0
 
 #BOOLEANAS
 var shop : bool = false
@@ -19,14 +20,20 @@ func podeTeia():
 	await get_tree().create_timer(cdTeia).timeout
 	vaiTeia = true
 
+func cuspir():
+	var projetil = sceneTeia.instantiate()
+	get_parent().add_child(projetil)
+	projetil.position = position
+	projetil.rotation = rotation
+
 func _physics_process(delta: float) -> void:
 	#Movimentacao da CAMERA
 	#if position.y <= cam.position.y:
 	cam.position.y = position.y
 	
 	#TEIA
-	if vaiTeia:
-		pass
+	if vaiTeia and Input.is_action_just_pressed("ui_accept") and !shop:
+		cuspir()
 	
 	#Acesso a LOJA
 	if shop:
@@ -41,12 +48,12 @@ func _physics_process(delta: float) -> void:
 		
 	input_vector = input_vector.normalized()
 	
-	velocity = input_vector * SPEED
+	velocity = input_vector * speed
 	move_and_slide()
 	
 	if input_vector.length() > 0.1:
 		var direction = input_vector.angle()
-		rotation = lerp_angle(rotation, direction, spRotation * delta)
+		rotation = lerp_angle(rotation, direction, speedRotation * delta)
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	shop = true
