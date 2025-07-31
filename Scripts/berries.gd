cfd1c9771a535d4a54548a4bd255bc20d2c5bf47
Thaymoz <artemiorabello123@gmail.com>
2025-07-31 16:42:24 -0300

    Efeitos visuais com frutinha e teia

--- Scripts/berries.gd
+++ Scripts/berries.gd
@@ -4,9 +4,19 @@ var capturado : bool = false
 
 var posiTeia : Area2D
 
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var anim_sprite: AnimatedSprite2D = $anim_sprite
+var escolha_da_frutinha : int 
 
+func _ready() -> void:
+	escolha_da_frutinha = randi_range(1,4)
+	if escolha_da_frutinha == 1:
+		anim_sprite.play("acerola")
+	elif escolha_da_frutinha == 2:
+		anim_sprite.play("amora")
+	elif escolha_da_frutinha == 3:
+		anim_sprite.play("framboesa")
+	elif escolha_da_frutinha == 4:
+		anim_sprite.play("mirtilo")
 func _physics_process(delta: float) -> void:
 	if capturado:
 		position = posiTeia.global_position
