43ad7e481f6fde0d9285faef9e5290ede5f6d4d0
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-08-01 01:12:31 -0300

    update goma

--- Scripts/goma.gd
+++ Scripts/goma.gd
@@ -6,6 +6,9 @@ func _ready() -> void:
 	var rot = randf_range(0, 360)
 	sprite.rotation = rot
 	
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Aranhinha":
 		body.nChiclete += 1
