bb4425c282e1ce726a63307533ac12b5b7721ea0
Thaymoz <artemiorabello123@gmail.com>
2025-08-01 18:28:38 -0300

    pause menu, chuva , limpador , sapo FINALIZADO

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+@onready var resume_btn: Button = $VBoxContainer/resume_btn
+
+func _ready() -> void:
+	visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_focus_next"):
+		visible = true
+		get_tree().paused = true
+		resume_btn.grab_focus()
+
+func _on_resume_btn_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+func _on_bm_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/tela_inicial.tscn")
