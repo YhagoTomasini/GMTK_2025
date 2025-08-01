06cd489baef91006b0c671cba3db6da454f21663
Thaymoz <artemiorabello123@gmail.com>
2025-08-01 14:43:44 -0300

    Sapo melhorado

--- Scripts/sapo.gd
+++ Scripts/sapo.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-@onready var damage_area: RayCast2D = $damage_area
 @onready var anim: AnimationPlayer = $anim
 @onready var timer: Timer = $Timer
 
@@ -8,6 +7,10 @@ var vivo : bool = true
 
 func _ready() -> void:
 	timer.wait_time = randf_range(2.0, 8.0)
+	
+
+
+	
 func _process(_delta: float) -> void:
 	#if damage_area.is_colliding():
 		#print("papou a aranha")
@@ -16,12 +19,6 @@ func _process(_delta: float) -> void:
 func sapo_ao_ataque():
 	if vivo:
 		anim.play("linguada")
-		if damage_area.is_colliding():
-			#var target = damage_area.get_collider()
-			#
-			#if target.name == "Aranhinha":
-				get_tree().reload_current_scene()
-				print("papou a aranha")
 	else:
 		anim.play("calado")
 	
@@ -36,3 +33,10 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.name == "Teia":
 		anim.play("calado")
 		vivo = false
+		
+func _on_linguinha_body_entered(body: Node2D) -> void:
+	if body.name == "Aranhinha":
+		get_tree().call_deferred("reload_current_scene")
+		print("papou a aranha")
+	else:
+		pass
