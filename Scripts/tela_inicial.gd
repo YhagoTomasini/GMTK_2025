668887b4b9356b2cfda7dbd6c1748cdef1b5f9a9
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-08-01 12:46:30 -0300

    update tela inicial prject settings

--- Scripts/tela_inicial.gd
+++ Scripts/tela_inicial.gd
@@ -9,10 +9,8 @@ func _ready() -> void:
 	startButton.grab_focus()
 	controls_panel.visible = false
 
-
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_start_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scene/main.tscn")
